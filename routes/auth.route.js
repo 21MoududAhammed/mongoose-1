468fe5766310e2fb613eb51309241e85513ec7d0
21MoududAhammed <21moudud@gmail.com>
2024-08-26 20:20:54 +0600

    register and login process done primarily

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -16,9 +16,30 @@ router.post("/register", async (req, res) => {
       email: req.body.email,
       isAdmin: req.body.isAdmin,
     });
-    res.status(201).json({ message: "success", data: result });
+    const { password, ...others } = result;
+    res.status(201).json({ message: "success", data: others });
   } catch (err) {
     console.log(err);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const user = await userModel.findOne({ userName: req.body.userName });
+  if (!user) {
+    return res.status().json({ message: "User not found." });
+  }
+  const decryptedPassword = CryptoJS.AES.decrypt(
+    user.password,
+    process.env.CRYPTO_SECRET
+  );
+  const genuinePassword = decryptedPassword.toString(CryptoJS.enc.Utf8);
+  const { password, ...others } = user._doc;
+  if (req.body.password === genuinePassword) {
+    // password matches
+    return res.status(400).json({ message: "Login Successful", user: others });
+  } else {
+    // password doesn't match
+    return res.status(401).json({ message: "Incorrect Password." });
   }
 });
 
